dffefb22f9c7112dd4892eb2c57e0f93c7e5394a
Ben Tenzlinger <btenzlin@1-C-27.42heilbronn.de>
2022-09-05 18:39:14 +0200

    module00 - ex00 more like c++

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -6,43 +6,21 @@
 /*   By: btenzlin <btenzlin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/05 15:16:40 by btenzlin          #+#    #+#             */
-/*   Updated: 2022/09/05 18:13:22 by btenzlin         ###   ########.fr       */
+/*   Updated: 2022/09/05 18:34:24 by btenzlin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-static void	str_to_caps(char *str)
-{
-	int	i;
-
-	if (str)
-	{
-		i = 0;
-		while (str[i])
-		{
-			std::cout << (char)toupper(str[i]);
-			i++;
-		}
-	}
-}
-
 int	main(int argc, char **argv)
 {
-	int	i;
-
 	if (argc == 1)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 	else
 	{
-		i = 1;
-		while (argv[i])
-		{
-			str_to_caps(argv[i]);
-			if (argv[i + 1])
-				std::cout << " ";
-			i++;
-		}
+		for(int i = 1; i < argc; i++)
+			for(int j = 0; argv[i][j] != '\0'; j++)
+				std::cout << (char)std::toupper(argv[i][j]);
 		std::cout << std::endl;
 	}
 	return (0);
