4c2e2f8aa564e8467ed9f9e22d5943c88c6e6aad
Ben Tenzlinger <btenzlin@1-C-27.42heilbronn.de>
2022-09-09 13:58:33 +0200

    SEARCH command almost finished except for some bugs

--- cpp00/ex01/includes/phonebook.hpp
+++ cpp00/ex01/includes/phonebook.hpp
@@ -6,7 +6,7 @@
 /*   By: btenzlin <btenzlin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/06 15:45:53 by btenzlin          #+#    #+#             */
-/*   Updated: 2022/09/08 16:06:54 by btenzlin         ###   ########.fr       */
+/*   Updated: 2022/09/09 13:53:10 by btenzlin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,14 +24,18 @@ public:
 	PhoneBook(void);
 	~PhoneBook(void);
 
-	void	set_infos(void);
-	void	show_contacts(void);
+	void		set_infos(void);
+	void		show_contacts(void);
 
 private:
 
-	Contact	contacts[8];
-	int		_index;
-	int		_amount_contacts;
+	Contact		contacts[8];
+	int			_index;
+	int			_amount_contacts;
+
+	std::string	get_info(std::string prompt);
+	void		print_info(std::string info);
+	void		show_contact_info(int index);
 };
 
 #endif
--- cpp00/ex01/src/phonebook.cpp
+++ cpp00/ex01/src/phonebook.cpp
@@ -6,7 +6,7 @@
 /*   By: btenzlin <btenzlin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/06 15:58:52 by btenzlin          #+#    #+#             */
-/*   Updated: 2022/09/08 16:24:50 by btenzlin         ###   ########.fr       */
+/*   Updated: 2022/09/09 13:53:31 by btenzlin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 PhoneBook::PhoneBook(void) : _index(0), _amount_contacts(-1) {}
 PhoneBook::~PhoneBook(void){}
 
-std::string	get_info(std::string prompt)
+std::string	PhoneBook::get_info(std::string prompt)
 {
 	std::string	buf;
 
@@ -37,13 +37,14 @@ void		PhoneBook::set_infos(void)
 	this->contacts[this->_index].set_secret(get_info("Enter darkest secret: "));
 	// std::cout << this->contacts[this->_index].get_phone_number() << std::endl;
 	this->_index++;
-	if (this->_index == 8)
+	if (this->_index == 3)
 		this->_index = 0;
-	if (this->_amount_contacts <= 7)
+	if (this->_amount_contacts < 2)
 		this->_amount_contacts++;
+	std::cout << this->_amount_contacts << std::endl;
 }
 
-void	print_info(std::string info)
+void	PhoneBook::print_info(std::string info)
 {
 	std::cout << std::setw(10);
 	if (info.size() > 10)
@@ -54,15 +55,50 @@ void	print_info(std::string info)
 	std::cout << info << "|";
 }
 
+void	PhoneBook::show_contact_info(int index)
+{
+	if (index >= 0 && index <= this->_amount_contacts)
+	{
+		std::cout << "Index: " << std::to_string(index) << std::endl;
+		std::cout << "First name: " << this->contacts[index].get_first_name() << std::endl;
+		std::cout << "Last name: " << this->contacts[index].get_last_name() << std::endl;
+		std::cout << "Nickname: " << this->contacts[index].get_nickname() << std::endl;
+		std::cout << "Phone number : " << this->contacts[index].get_phone_number() << std::endl;
+		std::cout << "Darkest secret: " << this->contacts[index].get_secret() << std::endl;
+	}
+	else
+		std::cout << "Invalid input or contact field empty. Please enter a number between 0 and " << this->_amount_contacts << std::endl;
+}
+
 void	PhoneBook::show_contacts(void)
 {
-	for (int i = 0; i <= this->_amount_contacts; i++)
+	int	input = -1;
+
+	if (this->_amount_contacts == -1)
 	{
-		std::cout << "|";
-		print_info(std::to_string(i));
-		print_info(this->contacts[i].get_first_name());
-		print_info(this->contacts[i].get_last_name());
-		print_info(this->contacts[i].get_nickname());
-		std::cout << std::endl;
+		std::string       result = "                      ___\n                _,-\'\"\"   \"\"\"\"`--.\n             ,-'          __,,-- \\\n           ,'    __,--\"\"\"\"dF      )\n          /   .-\"Hb_,--\"\"dF      /\n        ,'       _Hb ___dF\"-._,-'\n      ,'      _,-\"\"\"\"   \"\"--..__\n     (     ,-\'                  `.\n      `._,\'     _   _             ;\n       ,\'     ,\' `-\'Hb-.___..._,-\'\n       \\    ,\'\"Hb.-\'HH`-.dHF\"\n        `--\'   \"Hb  HH  dF\"\n                \"Hb HH dF\n                 \"HbHHdF\n                  |HHHF\n                  |HHH|\n                  |HHH|\n                  |HHH|\n                  |HHH|\n                  dHHHb\n                .dFd|bHb.               o\n      o       .dHFdH|HbTHb.          o /\n\\  Y  |  \\__,dHHFdHH|HHhoHHb.___Ben   Y\n##########################################";
+		std::cout << result << std::endl;
+		std::cout << "Contact list empty. Add new contact using the \'ADD\' command." << std::endl;
+	}
+	else
+	{
+		std::cout << "|-------------------------------------------|" << std::endl;
+		std::cout << "|     Index|First Name| Last Name|  Nickname|" << std::endl;
+		std::cout << "|----------|----------|----------|----------|" << std::endl;
+		for (int i = 0; i <= this->_amount_contacts; i++)
+		{
+			std::cout << "|";
+			print_info(std::to_string(i));
+			print_info(this->contacts[i].get_first_name());
+			print_info(this->contacts[i].get_last_name());
+			print_info(this->contacts[i].get_nickname());
+			std::cout << std::endl;
+		}
+		while (input < 0 || input > this->_amount_contacts)
+		{
+			std::cout << "Choose contact by entering corresponding index: ";
+			std::cin >> input;
+			show_contact_info(input);
+		}
 	}
 }
