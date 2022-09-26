d390745b3b335867a0e46f95fea2bb0efdffe81b
Ben Tenzlinger <btenzlin@1-D-29.42heilbronn.de>
2022-09-26 16:26:59 +0200

    module 01 - ex04

---
+++ cpp01/ex04/src/main.cpp
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: btenzlin <btenzlin@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/26 13:00:02 by btenzlin          #+#    #+#             */
+/*   Updated: 2022/09/26 16:23:03 by btenzlin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iomanip>
+#include <iostream>
+#include <fstream>
+
+int	main(int argc, char **argv)
+{
+	std::string		buf;
+	std::string		name;
+	std::fstream	file(argv[1]);
+	int				pos;
+
+	if (argc != 4)
+		std::cerr << "Error: invalid arguments." << std::endl;
+	else if (!std::string(argv[2]).compare(""))
+		std::cerr << "Error: empty search string." << std::endl;
+	else if (file.is_open())
+	{
+		name = std::string(argv[1]).append(".replace");
+		std::ofstream	newFile (name);
+		while (std::getline(file, buf))
+		{
+			pos = buf.find(argv[2]);
+			while (pos != -1)
+			{
+				buf.erase(pos, strlen(argv[2]));
+				buf.insert(pos, argv[3]);
+				pos = buf.find(argv[2], pos + strlen(argv[3]));
+			}
+			newFile << buf << std::endl;
+		}
+	}
+	else
+		std::cerr << "Error: invalid file." << std::endl;
+	return (0);
+}
