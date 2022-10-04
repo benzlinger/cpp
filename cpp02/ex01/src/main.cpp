b57c7b39f9a8de2a4ab3694377cff12db50bfbfd
Ben Tenzlinger <btenzlin@1-D-29.42heilbronn.de>
2022-10-04 20:19:35 +0200

    changed test to subjects example

--- cpp02/ex01/src/main.cpp
+++ cpp02/ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: btenzlin <btenzlin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/27 19:43:24 by btenzlin          #+#    #+#             */
-/*   Updated: 2022/09/29 14:28:11 by btenzlin         ###   ########.fr       */
+/*   Updated: 2022/10/04 20:18:32 by btenzlin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,11 @@
 int	main(void)
 {
 	Fixed a;
-	Fixed const b( 10 );
-	Fixed const c( 42.79f );
-	Fixed const d( b );
+	Fixed const b(10);
+	Fixed const c(42.42f);
+	Fixed const d(b);
 
-	a = Fixed( 1234.4321f );
+	a = Fixed(1234.4321f);
 
 	std::cout << "a is " << a << std::endl;
 	std::cout << "b is " << b << std::endl;
