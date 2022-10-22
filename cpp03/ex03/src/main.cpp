056a6d0edea99e98176c2b264748f966673e8265
Ben Tenzlinger <btenzlin@1-D-29.42heilbronn.de>
2022-10-22 18:46:57 +0200

    changed tests

--- cpp03/ex03/src/main.cpp
+++ cpp03/ex03/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: btenzlin <btenzlin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/05 11:45:09 by btenzlin          #+#    #+#             */
-/*   Updated: 2022/10/20 11:55:08 by btenzlin         ###   ########.fr       */
+/*   Updated: 2022/10/20 12:33:49 by btenzlin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,6 @@
 int	main(void)
 {
 	DiamondTrap	bob("Bob");
-	DiamondTrap	bert;
 
 	std::cout << "Name: " << bob.getName() << std::endl;
 	std::cout << "Hp: " << bob.getHp() << std::endl;
@@ -26,15 +25,5 @@ int	main(void)
 	std::cout << "Dmg: " << bob.getDmg() << std::endl;
 	bob.whoAmI();
 	bob.attack("Mirko");
-	bob.takeDamage(50);
-	bob.beRepaired(25);
-	bob.guardGate();
-	bob.highFiveGuys();
-	bob.whoAmI();
-	std::cout << "Name: " << bert.getName() << std::endl;
-	std::cout << "Hp: " << bert.getHp() << std::endl;
-	std::cout << "Ep: " << bert.getEp() << std::endl;
-	std::cout << "Dmg: " << bert.getDmg() << std::endl;
-	bert.whoAmI();
 	return (0);
 }
